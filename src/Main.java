8b8a4f1f89234ea8d6c5815a191fa98ade3151b5
MeHunter2004 <120305394+MeHunter2004@users.noreply.github.com>
2023-08-14 16:59:13 +0330

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Account account1 = new Account("Mostafa");
+        Account account2 = new Account("sajedeh");
+        Account account3 = new Account("simin");
+
+        System.out.println("Account1 name is : \n" + account1.getName());
+        System.out.println("Account2 name is : \n" + account2.getName());
+        System.out.println("Account3 name is : \n" + account3.getName());
+
     }
 }
